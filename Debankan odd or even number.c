77c505f8579c4d9ad28b6c439daa084f4ea7d883
Debankan <43395875+Debankan@users.noreply.github.com>
2018-09-19 11:52:09 +0530

    Add files via upload

---
+++ Debankan odd or even number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+	printf("Enter the number to be checked");
+	int num ;
+	scanf("%d", &num);
+if(num %2 == 0)
+{ 
+  printf("the number is even");
+	}
+	else
+	{ 
+	 printf("the number is odd");
+		}
+		return 0;
+		
+}
