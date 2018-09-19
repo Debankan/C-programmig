060b1eacf0844239126ada2bd69e9cf884b42e6e
Debankan <43395875+Debankan@users.noreply.github.com>
2018-09-19 11:52:24 +0530

    Add files via upload

---
+++ Debankan C project.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,sum ;
+	printf("enter a and b");
+	scanf("%d %d", &a ,&b);
+	sum=a+b;
+	printf("the sum of %d and %d is %d", a , b , sum);
+	return 0 ;
+	
+}
+ 
