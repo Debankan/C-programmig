5221e92026c0a6bda3c224ed16b1f6786f0ca551
Debankan <43395875+Debankan@users.noreply.github.com>
2018-09-23 15:32:18 +0530

    Update debankan 2.c

--- debankan 2.c
+++ debankan 2.c
@@ -2,14 +2,13 @@
 #include<conio.h>
 int main()
 {
-	int a,b,c;
-	printf("enter a and b");
-	scanf("%d %d", &a ,&b);
-	c=a;
-	a=b;
-	b=c;
-	printf("After swapping the value of a and b ");
-	printf("%d %d", a,b);
+	int num1,num2,c;
+	printf("enter num1 and num2");
+	scanf("%d %d", &num1,&num2);
+	c=num1;
+	num1=num2;
+	num2=c;
+	printf("After swapping the value of num1 and num2 is %d %d",num1 ,num2);
 	return 0 ;
 	
 }
