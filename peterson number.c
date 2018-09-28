6a07c0572b1b52ae041500d2977b43b99d446b72
Debankan <43395875+Debankan@users.noreply.github.com>
2018-09-28 11:32:59 +0530

    Add files via upload

--- peterson number.c
+++ peterson number.c
@@ -1,26 +1,33 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
-{
-int n,i,no,sum=0,d,p;
-printf("Enter the number:");
-scanf("%d",&n);
-no=n;
-while(n>0)
-{
-d=n%10;
-n=n/10;
-for(i=1,p=1;i<=d;i++)
-p=p*i;
-sum=sum+p;
-}
-if(sum==no)
-{
-printf("%d is a Peterson number",no);
-}
-else
-{
-printf("%d not a Peterson number",no);
-}
-return 0;
+ int main()
+   {
+   	 int i,n,p,d,no,sum;
+   	 sum=0;
+   	 printf(" Enter The number\n\n");
+   	 scanf("%d",&n);
+   	 no=n;
+   	 while(n>0)
+   	    {
+   	      d=n%10;
+   	      n=n/10;
+   	      p=1;
+   	    for(i=1;i<=d;i++)
+   	       {
+   	       	
+   	       	p=p*i;
+   	       }
+   	       	sum=sum+p;
+	}
+		   
+		   
+		    if(sum==no)
+		      {
+		      	printf("%d is a peterson number\n\n",sum);
+			  }
+		else 
+		    {
+		    	printf("%d is not a peterson number\n\n",sum);
+				  }
+        return 0 ;
 }
